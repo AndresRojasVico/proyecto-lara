ca707652b70ab68ecfda9329eb60edee604e8f8e
Andres <rojasvico@hotmail.com>
2024-12-19 07:22:49 +0100

    migracion user ok

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 return new class extends Migration
 {
@@ -11,15 +13,37 @@ return new class extends Migration
      */
     public function up(): void
     {
+        /*
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
+            $table->string('nick');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+        */
+
+        DB::statement("
+         CREATE TABLE IF NOT EXISTS users (
+    id              int(255) auto_increment not null,
+    role            varchar(20),
+    name            varchar(100),
+    surname         varchar(200),
+    nick            varchar(100),
+    email           varchar(255),
+    password        varchar(255),
+    image           varchar(255),
+    created_at      datetime,
+    updated_at      datetime,
+    remember_token  varchar(255),
+
+    CONSTRAINT pk_user PRIMARY KEY(id)
+)ENGINE=InnoDB ;
+         ");
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
